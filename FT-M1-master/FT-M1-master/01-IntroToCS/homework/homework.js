810e1ea634103543b30edc6b441d65305fcfc865
JmonteroDev <julianmontero644@gmail.com>
2023-03-02 06:23:44 -0500

    first exercise result introToCs

--- FT-M1-master/FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/FT-M1-master/01-IntroToCS/homework/homework.js
@@ -1,14 +1,25 @@
 'use strict';
 
 function BinarioADecimal(num) {
-   var num = 2;
-   console.log(num) 
+   console.log(num);
+   var bina = num.split("").reverse().join("");
+   console.log(bina);
 
+   var suma = 0;
+
+   for (let i = 0; i < bina.length; i++) {
+      suma += bina[i] * 2 ** i;
+      console.log(suma);
+   }
+   console.log(suma);
+   return suma;
 }
 
 function DecimalABinario(num) {
 
 }
+
+BinarioADecimal("10");
 
 module.exports = {
    BinarioADecimal,
