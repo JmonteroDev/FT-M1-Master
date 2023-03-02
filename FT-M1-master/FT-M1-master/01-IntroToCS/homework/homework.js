508a9e5715d029fef7bcce2319b049f90c45b0b1
JmonteroDev <julianmontero644@gmail.com>
2023-03-02 05:23:52 -0500

    first commit

---
+++ FT-M1-master/FT-M1-master/01-IntroToCS/homework/homework.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function BinarioADecimal(num) {
+   var num = 2;
+   console.log(num) 
+
+}
+
+function DecimalABinario(num) {
+
+}
+
+module.exports = {
+   BinarioADecimal,
+   DecimalABinario,
+};
